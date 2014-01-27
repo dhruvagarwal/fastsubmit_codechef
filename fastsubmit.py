3bb6304a81a745fab5093dd5b443e8792c486dc9
dhruvagarwal <dhruvagga@gmail.com>
2014-01-27 14:02:29 +0530

    spoj feature added

--- fastsubmit.py
+++ fastsubmit.py
@@ -1,21 +1,25 @@
 import mechanize
 
-def f():
+def f(site):
 	path=raw_input('Give your file path along with name and extension : ')
 	fil=open(path).read()
-	br.open('http://www.codechef.com/submit/'+raw_input("Enter Question Code : "))
+	br.open('http://www.'+site+'.com/submit/'+raw_input("Enter Question Code : "))
 	br.select_form(nr=0)
-	br.form["body"]=fil
+	br.form["body" if site=='codechef' else "file"]=fil
 	br.submit()
 	
 br=mechanize.Browser()
 br.set_handle_robots(False)
-br.open('http://www.codechef.com')
-br.select_form(nr=0)
-br.form["name"]=raw_input("Enter handle/Username : ")
-br.form["pass"]=raw_input("Enter password : ")
-br.submit()
-ch='y'
-while ch=='y':
-	f()
-	ch=raw_input("do you want to continue(y/n) : ")
+site=raw_input("Enter site's name (codechef/spoj) : ").lower()
+if site in ['codechef','spoj']:
+	br.open('http://www.'+site+'.com')
+	br.select_form(nr=0)
+	br.form["name" if site=='codechef' else "login_user"]=raw_input("Enter handle/Username : ")
+	br.form["pass" if site=='codechef' else "password"]=raw_input("Enter password : ")
+	br.submit()
+	ch='y'
+	while ch=='y':
+		f(site)
+		ch=raw_input("do you want to continue(y/n) : ")
+
+	
