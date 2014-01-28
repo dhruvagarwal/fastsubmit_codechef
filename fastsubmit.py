18690b493bd6ea9356ebd9abf6905795fd1bf404
dhruvagarwal <dhruvagga@gmail.com>
2014-01-28 21:00:32 +0530

    updated codechef status checking

--- fastsubmit.py
+++ fastsubmit.py
@@ -9,6 +9,16 @@ def f_submit(site):
 	br.form["body" if site=='codechef' else "file"]=fil
 	return br.submit()
 
+def recheck_cc(solution_id):
+	response = mechanize.urlopen('http://www.codechef.com/viewsolution/'+solution_id)
+	parser = BeautifulSoup(str(response.read()))
+	div_list = parser.find_all('div', class_='head')
+	for divs in div_list:
+		for content in divs.contents:
+			if str(content).find('Status') is not -1:
+				status = str(content).split(',')[0]
+				return status.split(':')[1]
+
 def cc():
     response = f_submit('codechef')
     html_code = str(response.read())
@@ -19,16 +29,10 @@ def cc():
             tokens = line.split()
             solution_id = tokens[3].split(';')[0]
             print solution_id
-            response = mechanize.urlopen('http://www.codechef.com/viewsolution/'+solution_id)
-
-            parser = BeautifulSoup(str(response.read()))
-            div_list = parser.find_all('div', class_='head')
-            for divs in div_list:
-                for content in divs.contents:
-                    if str(content).find('Status') is not -1:
-                        status = str(content).split(',')[0]
-                        print status.split(':')[1]
-
+            res=recheck_cc(solution_id)
+            while '??' in res:
+            	res=recheck_cc(solution_id)
+            print res
 def recheck(user):
 	c=0
 	prob_list=mechanize.urlopen('http://www.spoj.com/status/'+user+'/signedlist').read().split('\n')
